cf50e9b272924b69e7c66ae5c82f0811bbf494f0
leonelgarcia <leogarcia@uade.edu.ar>
2025-08-30 17:22:01 -0300

    creacion de funciones para ingresar 20 equipos

--- main.py
+++ main.py
@@ -1,5 +1,31 @@
 
-print("Hello World")
 
+# ********************************** IMPORT FUNCIONES PYTHON********************
+
+from random import randint
+
+# ********************************** IMPORT FUNCIONES PROPIAS ******************
+
+from obtener_equipos import obtener_equipos
+from mostrar_equipos import mostrar_equipos
+
+# ***************************** VARIABLES GLOBALES *****************************
+CANTIDAD_EQUIPOS = 20
+
+# ***************************** FUNCION MAIN ***********************************
+def main():
+ print("SISTEMA DE GESTIÓN DE EQUIPOS DE FÚTBOL")
+ print("======================================")
+ 
+ # Obtener la lista de equipos
+ equipos = obtener_equipos()
+ 
+ # Mostrar la lista de equipos
+ mostrar_equipos(equipos)
+ 
+
+# **************************** LLAMADO A FUNCION MAIN **************************
+
+main()
 
 
